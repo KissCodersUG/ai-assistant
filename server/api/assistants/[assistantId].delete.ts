2c5f81174729318b7125ffa261db6f185ae8980f
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2024-03-28 23:14:13 +0000

    Implement DELETE assistants endpoint

---
+++ server/api/assistants/[assistantId].delete.ts
@@ -0,0 +1,7 @@
+import { openAIService } from '../../services/OpenAIService';
+
+export default defineEventHandler(async (event) => {
+  const { assistantId } = getRouterParams(event);
+  const { openAiApiKey } = useRuntimeConfig();
+  return openAIService.deleteAssistant(openAiApiKey, assistantId);
+});
