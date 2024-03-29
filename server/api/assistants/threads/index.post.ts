97c12dfb9f88934f3077d008ee140d3f6e47121a
Marcel Weeske <marcel.weeske@kisscoders.ruhr>
2024-03-29 17:04:19 +0100

    implements createThread

---
+++ server/api/assistants/threads/index.post.ts
@@ -0,0 +1,6 @@
+import { openAIService } from '../../../services/OpenAIService';
+
+export default defineEventHandler(async () => {
+  const { openAiApiKey } = useRuntimeConfig();
+  return openAIService.createThread(openAiApiKey);
+});
