ad7c7ae4fce6e80d51f454485c1377f6795a421a
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2024-04-01 20:28:07 +0100

    feat: test sse

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
 const { signOut } = useAuth();
+
+type message = {
+  id: string;
+  message: string;
+};
+
+const messages = ref<message[]>([]);
+const sseUrl = "/api/sse";
+onMounted(() => {
+  const eventSource = new EventSource(sseUrl);
+
+  eventSource.onmessage = function (event) {
+    // Parse the incoming message and add it to the messages array
+    const message = JSON.parse(event.data) as message;
+    messages.value.push(message);
+  };
+
+  eventSource.onerror = function (error) {
+    console.error("EventSource failed:", error);
+    eventSource.close();
+  };
+
+  onUnmounted(() => {
+    console.log("closing event source");
+    eventSource.close();
+  });
+});
 </script>
 
 <template>
@@ -13,6 +40,12 @@ const { signOut } = useAuth();
       >
         Sign Out
       </button>
+    </div>
+    <div
+      v-for="msg in messages"
+      :key="msg.id"
+    >
+      {{ msg }}
     </div>
   </div>
 </template>
---
+++ server/api/sse.ts
@@ -0,0 +1,46 @@
+import { H3Event } from "h3";
+import { createHooks } from "hookable";
+
+export interface ServerSentEvent {
+  [key: string]: <T, R>(data: T) => R | void;
+}
+
+const sseHooks = createHooks<ServerSentEvent>();
+
+export const useSSE = (event: H3Event, hookName: string) => {
+  setHeader(event, "content-type", "text/event-stream");
+  setHeader(event, "cache-control", "no-cache");
+  setHeader(event, "connection", "keep-alive");
+  setResponseStatus(event, 200);
+
+  let id = 0;
+
+  sseHooks.hook(hookName, (data: any) => {
+    event.node.res.write(`id: ${(id += 1)}\n`);
+    event.node.res.write(`data: ${JSON.stringify(data)}\n\n`);
+    event.node.res.flushHeaders();
+  });
+
+  const send = (callback: (id: number) => any) => {
+    sseHooks.callHook(hookName, callback(id));
+  };
+
+  const close = () => {
+    event.node.res.end();
+  };
+
+  event._handled = true;
+  event.node.req.on("close", close);
+
+  return { send, close };
+};
+
+export default defineEventHandler(async (event) => {
+  const { send } = useSSE(event, "sse:event");
+
+  const interval = setInterval(() => {
+    send((id) => ({ id, message: "Hello!" }));
+  }, 1000);
+
+  event.node.req.on("close", () => clearInterval(interval));
+});
